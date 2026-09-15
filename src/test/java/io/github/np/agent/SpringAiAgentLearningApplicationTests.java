a09ae4234751f3ed3ac4f16e6c8e2898f1896309
ethan-ning <11447657+ethan-ning@user.noreply.gitee.com>
2026-09-15 08:55:59 +0800

    init project

---
+++ src/test/java/io/github/np/agent/SpringAiAgentLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.np.agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiAgentLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
